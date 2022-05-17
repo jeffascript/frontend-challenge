f2937e3919363320bacaf0a9e142f74340bd158e
jeffascript <ofoaro.jeff@gmail.com>
2022-05-17 17:00:10 +0200

    fix: swap row values with Dummydata

--- src/views/user/UserHistoryView.tsx
+++ src/views/user/UserHistoryView.tsx
@@ -6,7 +6,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { makeStyles } from '@mui/styles';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback, useEffect } from 'react';
+
+import dummyData from '../../../dummyData.json';
+import { getMonthName } from './UserDashboardView';
 
 interface HistoryDataProps {
   year: number;
@@ -15,39 +18,44 @@ interface HistoryDataProps {
   reward: number;
 }
 
+type HistoryKeysArr = Array<keyof HistoryDataProps>;
+
 const useStyles = makeStyles(() => ({
   dashboardWrapper: {
     width: '100%',
   },
 }));
 
-const rows = [
-  { year: 2021, month: 12, co2_saved: 560.0, reward: 599 },
-  { year: 2021, month: 5, co2_saved: 37.5, reward: 0 },
-  { year: 2022, month: 1, co2_saved: 232.0, reward: 605 },
-  { year: 2022, month: 2, co2_saved: 340, reward: 12 },
-  { year: 2022, month: 3, co2_saved: 0, reward: 0 },
-  { year: 2022, month: 4, co2_saved: 543.0, reward: 599 },
-];
-
-// get the keys of the objects in the array
-const getKeys = (arr: HistoryDataProps[]) => Object.keys(arr[0]);
-
-const tabelHeaders = getKeys(rows);
-// sort by year and month
-const sortByYearAndMonth = (a: HistoryDataProps, b: HistoryDataProps) => {
-  if (Number(a.year) - Number(b.year) != 0) {
-    return b.year - a.year;
-  }
-  return b.month - a.month;
-};
-
-const sortedRows = rows.sort(sortByYearAndMonth);
-
-console.log(sortedRows);
-
 export const UserHistoryView = (): ReactElement => {
   const classes = useStyles();
+  const [historyData, setHistoryData] = React.useState<HistoryDataProps[]>([]);
+  const [historyKeys, setHistoryKeys] = React.useState<HistoryKeysArr>([]);
+
+  // callback for getting the keys of the objects in the array
+  const getKeys = useCallback((arr: HistoryDataProps[]) => Object.keys(arr[0]), []);
+
+  // sort callback =>  by year and month
+  const sortByYearAndMonth = useCallback((a: HistoryDataProps, b: HistoryDataProps) => {
+    if (Number(a.year) - Number(b.year) != 0) {
+      return b.year - a.year;
+    }
+    return b.month - a.month;
+  }, []);
+
+  useEffect(() => {
+    const fetchStaticData = async () => {
+      const data = dummyData.HISTORY_DATA;
+      const sortedRows = data.sort(sortByYearAndMonth);
+      const keys = getKeys(data) as HistoryKeysArr;
+
+      setHistoryData(sortedRows);
+      setHistoryKeys(keys);
+    };
+
+    fetchStaticData();
+  }, []);
+
+  if (historyData.length === 0) return <p> Nothing to show for now</p>;
 
   return (
     <div className={classes.dashboardWrapper}>
@@ -55,7 +63,7 @@ export const UserHistoryView = (): ReactElement => {
         <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
-              {tabelHeaders.map((header) => (
+              {historyKeys.map((header) => (
                 <React.Fragment key={header}>
                   <TableCell>{header}</TableCell>
                 </React.Fragment>
@@ -63,13 +71,13 @@ export const UserHistoryView = (): ReactElement => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map(({ month, year, co2_saved, reward }) => (
+            {historyData.map(({ month, year, co2_saved, reward }) => (
               <TableRow
-                key={`${year}-${month}-${co2_saved}-${reward}`}
+                key={`${year}${month}${co2_saved}${reward}`}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell>{year}</TableCell>
-                <TableCell>{month}</TableCell>
+                <TableCell>{`${getMonthName(month)}`}</TableCell>
                 <TableCell>{co2_saved}</TableCell>
                 <TableCell>{reward}</TableCell>
               </TableRow>
